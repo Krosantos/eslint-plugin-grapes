eaf4564f76ffbcc9967210591ed024c4670e241b
Tymko Tecca <tymko.tecca@gmail.com>
2025-12-10 12:11:30 -0500

    Move to pnpm, update peerdep, add documentation link

--- rules/no-internal-import.js
+++ rules/no-internal-import.js
@@ -1,13 +1,13 @@
-const fs = require("fs");
-const path = require("path");
+const fs = require('fs');
+const path = require('path');
 const IMPORT_PREFIX = /(\.\.\/)+|(\.\/)+/;
 const BAD_IMPORT_REGEX = /(\.{1,2}\/)\w+\//g;
 const INDEX_FILE_REGEX = /^index\.(j|t)sx?$/g;
 
 const importIsModule = (importPath, contextPath, indexRegex) => {
   const prefix = importPath.match(IMPORT_PREFIX)[0];
-  const pathSuffix = importPath.replace(prefix, "");
-  const split = pathSuffix.split("/");
+  const pathSuffix = importPath.replace(prefix, '');
+  const split = pathSuffix.split('/');
   while (split.length > 1) {
     split.pop();
     const me = path.parse(contextPath);
@@ -29,8 +29,8 @@ const importIsModule = (importPath, contextPath, indexRegex) => {
 const create = function (context) {
   let indexRegex = INDEX_FILE_REGEX;
   const options = context.options || [];
-  if(options.length > 0){
-    const {indexFileRegex} = options[0];
+  if (options.length > 0) {
+    const { indexFileRegex } = options[0];
     indexRegex = new RegExp(indexFileRegex);
   }
   return {
@@ -38,18 +38,13 @@ const create = function (context) {
       if (!node || !node.source || !node.source.value) return;
       const importPath = node.source.value;
       if (!importPath.match(BAD_IMPORT_REGEX)) return;
-      
-      const isModule = importIsModule(
-        importPath,
-        context.getFilename(),
-        indexRegex
-      );
+
+      const isModule = importIsModule(importPath, context.getFilename(), indexRegex);
 
       if (isModule) {
         context.report({
           node,
-          message:
-            "Do not import another module's internal files or functionality.",
+          message: "Do not import another module's internal files or functionality.",
         });
       }
     },
@@ -57,23 +52,22 @@ const create = function (context) {
 };
 
 const meta = {
-  type: "suggestion",
+  type: 'suggestion',
   docs: {
-    description:
-      'You should not access another "unit"\'s internal files or functions.',
-    category: "Best Practices",
+    description: 'You should not access another "unit"\'s internal files or functions.',
+    category: 'Best Practices',
+    url: 'https://github.com/Krosantos/eslint-plugin-grapes?tab=readme-ov-file#rule-details',
     recommended: true,
   },
   schema: [
     {
-      type: "object",
+      type: 'object',
       properties: {
         indexFileRegex: {
-          type: "string",
-          title: "Index file regex",
-          description:
-            'The regex used to determine what constitutes an "index" file',
-          default: "^index\\.(j|t)sx?$",
+          type: 'string',
+          title: 'Index file regex',
+          description: 'The regex used to determine what constitutes an "index" file',
+          default: '^index\\.(j|t)sx?$',
         },
       },
       additionalProperties: false,
